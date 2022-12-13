4c2cb69f1effc63d47f5b11cec35caacb6007853
Guilherme Venâncio <103697225+guilhermevenancio27@users.noreply.github.com>
2022-12-13 17:26:57 -0300

    Add files via upload

---
+++ TR4_536423.c
@@ -0,0 +1,44 @@
+#include "ordvetor.h"
+
+/* essa seção do codigo tem função de criar o vetor ordenado */
+VETORORD* VETORD_create(int n, COMP* compara) {
+	VETORORD* vetor = malloc(sizeof(VETORORD));
+	vetor->N = n;
+	vetor->P = 0;
+	vetor->elems = malloc(n * sizeof(void*));
+	vetor->comparador = compara;
+	return vetor;
+}
+/* função de adicionar ao vetor*/ 
+void VETORD_add(VETORORD* vetor, void* newelem) {
+	if (vetor->P < vetor->N) {
+		int posicao = vetor->P;
+
+		for (int i = 0; i < vetor->P; i++) {
+			if (vetor->comparador(vetor->elems[i], newelem) == -1) {
+				posicao = i;
+				break;
+			}
+		}
+
+		for (int i = vetor->P; i > posicao; i--) {
+			vetor->elems[i] = vetor->elems[i - 1];
+		}
+
+		vetor->elems[posicao] = newelem;
+		vetor->P++;
+	}	
+}
+
+/* função de remover do vetor */
+void* VETORD_remove(VETORORD* vetor) {
+	void* menor = vetor->elems[0];
+
+	for (int i = 0; i < vetor->P - 1; i++) {
+		vetor->elems[i] = vetor->elems[i + 1];
+	}
+	
+	vetor->P--;
+
+	return menor;
+}
